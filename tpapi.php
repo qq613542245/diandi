81ba5d4e7ff788f54575b738fa6d4e22aed0708a
句号哥哥 <84637760+qq613542245@users.noreply.github.com>
2021-05-23 02:17:37 +0800

    Add files via upload

---
+++ tpapi.php
@@ -0,0 +1,8 @@
+<?php
+ //这将得到一个文件夹中的所有gif，jpg和png图片的数组 
+$img_array = glob("./manifest/*/*.{gif,jpg,png}",GLOB_BRACE);
+ //从数组中选择一个随机图片 
+$img = array_rand($img_array);
+//在页面显示图片 
+header("location:.$img_array[$img]"); 
+ ?>
