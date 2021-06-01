47e40eca641386f7cee08eccde94dc08fbd88910
句号哥哥 <84637760+qq613542245@users.noreply.github.com>
2021-06-01 18:41:40 +0800

    Update three_punch_combo.js

--- js/three_punch_combo.js
+++ js/three_punch_combo.js
@@ -1,4 +1,4 @@
-        $i = 0;
+   $i = 0;
         $('#start').click(function(){
             $i++;
             if($i >=6 ){
@@ -7,6 +7,6 @@
             }
         })
         $('#stop').click(function(){
-            alert('这么作？今天别吃了！')
+            alert('美食虽好,切忌贪吃，还是要按时吃好一日三餐！')
             $(this).hide();
         })
